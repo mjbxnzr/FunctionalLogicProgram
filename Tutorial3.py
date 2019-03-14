c4d3dc3c6702875482ea7ea4296e29e924bab45b
Muhd Mujib <muhdiqmal1997@gmail.com>
2019-03-14 11:50:18 +0800

    Extract integer inside a string in an array

---
+++ Tutorial3.py
@@ -0,0 +1,16 @@
+import re
+
+arr = [1,34,56,'98abc', 'd', -1, 0, '#', 34, 56, 'sds', 'ewe',
+       '34', 44, 1, 3, 'qbcd', 23, '345', '23', '-10', 10,
+       -10, 345, 45, '23', 'aer', '12ac', 'anc34', 'asas']
+
+IsInteger = [x for x in arr if type(x)==int]
+IsNotInteger = str([y for y in arr if type(y)!=int])
+
+for x in IsNotInteger:
+    extract = map(int,re.findall('\d+',IsNotInteger))
+
+combine=sorted(sorted(IsInteger)+sorted(extract))
+arr2=sorted(list(dict.fromkeys(combine)))
+
+print(arr2)
